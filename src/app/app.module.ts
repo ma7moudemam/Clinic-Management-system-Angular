b8ddbd33ec7542447c10c9e883eb6f950ebae60c
KarimShahin <karim.alaa.shahin@gmail.com>
2022-03-22 19:15:23 +0200

    creating medicine card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicineCardComponent } from './medicine/medicine-card/medicine-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicineCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/medicine/medicine-card/medicine-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medicine-card',
+  templateUrl: './medicine-card.component.html',
+  styleUrls: ['./medicine-card.component.css']
+})
+export class MedicineCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
