b6e050b410c45e4fb6980846ccc2ddadf6645e56
KarimShahin <karim.alaa.shahin@gmail.com>
2022-03-23 18:40:38 +0200

    medicine card last update

--- src/app/medicine/medicine-card/medicine-card.component.ts
+++ src/app/medicine/medicine-card/medicine-card.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Medicine } from '../medicine';
 
 @Component({
@@ -6,7 +14,7 @@ import { Medicine } from '../medicine';
   templateUrl: './medicine-card.component.html',
   styleUrls: ['./medicine-card.component.css'],
 })
-export class MedicineCardComponent implements OnInit {
+export class MedicineCardComponent implements AfterViewInit, OnInit {
   @Input() medicine: Medicine = new Medicine(
     0,
     'Panadol',
@@ -16,7 +24,34 @@ export class MedicineCardComponent implements OnInit {
     '',
     0
   );
+  isLong: boolean = false;
+  smallDescribtion: string;
+  restOfDescribtion: string;
+  @ViewChild('a') a: ElementRef;
+  @ViewChild('a2') a2: ElementRef;
+  @ViewChild('more') more: ElementRef;
   constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit() {
+    if (this.medicine.describtion.split(' ').length > 30) this.isLong = true;
+    this.smallDescribtion = this.medicine.describtion
+      .split(' ')
+      .slice(0, 30)
+      .join(' ');
+    this.restOfDescribtion = this.medicine.describtion
+      .split(' ')
+      .slice(30)
+      .join(' ');
+  }
+  ngAfterViewInit(): void {}
+  showHide() {
+    if (this.more.nativeElement.style.display == 'none') {
+      this.more.nativeElement.style.display = 'inline';
+      this.a2.nativeElement.style.display = 'inline';
+      this.a.nativeElement.style.display = 'none';
+    } else {
+      this.more.nativeElement.style.display = 'none';
+      this.a2.nativeElement.style.display = 'none';
+      this.a.nativeElement.style.display = 'inline';
+    }
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,33 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  userData: any = {}
+  userData: any = {};
   file: any;
-
-  constructor(public auth:AuthService, public router: Router) { }
+  @ViewChild('p') paragraph: ElementRef | undefined;
+  constructor(public auth: AuthService, public router: Router) {}
 
   ngOnInit(): void {
     // this.authSer
   }
 
   registerUser() {
-    this.auth.register(this.userData, this.file)
-    .subscribe((a:any) =>  {
+    this.auth.register(this.userData, this.file).subscribe((a: any) => {
       localStorage.setItem('token', (<any>a).token);
-      localStorage.setItem('role', (<any>a).role)
-      this.router.navigate(['/'])
-    })
+      localStorage.setItem('role', (<any>a).role);
+      this.router.navigate(['/']);
+    });
   }
-  onFileChange(s:any){
-    this.file=s.target.files[0];
-   
+  onFileChange(s: any) {
+    this.file = s.target.files[0];
   }
-
 }
